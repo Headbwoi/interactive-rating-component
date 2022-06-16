994fc115aad6c3b9bc5b40a84f15b069b827739f
headbwoi <tochukwujohn24@gmail.com>
2022-06-16 22:06:51 +0100

    finished config file

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+    },
+    fontFamily: {
+      overpass: ["'Overpass', 'sans-serif'"]
+    },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      lightGrey: "hsl(217, 12%, 63%)",
+      mediumGrey: "hsl(216, 12%, 54%)",
+      darkBlue: "hsl(213, 19%, 18%)",
+      darkBlueLight: "hsla(213, 19%, 18%, 0.6)",
+      veryDarkBlue: "hsl(216, 12%, 8%)",
+      orange: "hsl(25, 97%, 53%)"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
