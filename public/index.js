fc01d3a0a374dc7d2403ee389c682b73a09cb00b
headbwoi <tochukwujohn24@gmail.com>
2022-06-17 23:01:02 +0100

    adjusted design elements

--- public/index.js
+++ public/index.js
@@ -1,48 +1,28 @@
-const ratingCard = document.querySelector('.state-card');
-const thankYouCard = document.querySelector('.thank_you_card');
-const cardForm = document.getElementById('form');
-const ratings = cardForm.querySelectorAll('.rating');
-const card = document.querySelector('.card-1');
+const ratingCard = document.querySelector(".state-card")
+const thankYouCard = document.querySelector(".thank_you_card")
+const form = document.querySelector("#card-form")
+const wrapper = document.querySelector(".wrapper")
+const ratingNumber = document.querySelector("#rating-number")
 
-
-const removeActiveClass = () => {
-    ratings.forEach(card => {
-        card.classList.remove("active")
-    })
-}
-
-const addActiveClass = () => {
-    ratings.forEach(card => {
-        card.addEventListener("click", () => {
-            removeActiveClass()
-            card.classList.add("active")
-        })
-    })
-}
-
-cardForm.addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
+    const selectedRating = document.querySelector('input[type=radio]:checked').value;
+    if(selectedRating !== null) {
+        ratingNumber.innerHTML = selectedRating;
 
-    
-    if(ratings !== null || ratings !== undefined) {
-        const userSelection = document.getElementById('rating-number');
-        userSelection.innerHTML = ratings;
-
-        let flipAnimationDuration = 1800;
-        Promise.resolve(
-            setTimeout(() => {
-                card.classList.add("flip-animation");
-                ratingCard.classList.add("hidden");
-            }, 0)
-        ).then(
-            setTimeout(() => {
-                ratingCard.remove();
-                thankYouCard.classList.remove("hidden");
-                thankYouCard.classList.add("fade-bottom")
-            }, flipAnimationDuration)
-        )
-        }
+        
+                let flipAnimationDuration = 1800;
+                Promise.resolve(
+                    setTimeout(() => {
+                        wrapper.classList.add("flip-animation");
+                        ratingCard.classList.add("hidden");
+                    }, 0)
+                ).then(
+                    setTimeout(() => {
+                        ratingCard.remove();
+                        thankYouCard.classList.remove("hidden");
+                        thankYouCard.classList.add("fade-bottom")
+                    }, flipAnimationDuration)
+                )
     }
-)
-
-addActiveClass()
+})
